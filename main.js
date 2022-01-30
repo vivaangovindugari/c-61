270ea51c39ffb3c7f8806bdea42559bdc78c6641
vivaangovindugari <97267116+vivaangovindugari@users.noreply.github.com>
2022-01-29 19:34:36 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,56 @@
+function a()
+{
+    document.getElementById("w").src="https://i.ytimg.com/vi/5PpczSCmKpg/maxresdefault.jpg";
+}
+function ab()
+{
+    document.getElementById("w").src="crane.jpg";
+}
+function b()
+{
+    document.getElementById("w1").src="https://i0.wp.com/origami.guide/wp-content/uploads/2018/04/easy-origami-lotus-tutorial-00.jpg?fit=1920%2C1920&ssl=1";
+}
+function bb()
+{
+    document.getElementById("w1").src="tulip_flower.jpg";
+}
+function c()
+{
+    document.getElementById("w2").src="https://i.ytimg.com/vi/m7M5ckiUXbM/maxresdefault.jpg";
+}
+function cb()
+{
+    document.getElementById("w2").src="envelop.jpg";
+}
+function d()
+{
+    document.getElementById("w3").src="https://i0.wp.com/origami.guide/wp-content/uploads/2018/04/origami-masu-box-00.jpg?fit=1920%2C1920&ssl=1";
+}
+function db()
+{
+    document.getElementById("w3").src="modular_box.jpg";
+}  
+function e()
+{
+    document.getElementById("w4").src="https://www.thesprucecrafts.com/thmb/dK-xTGNd_WaWlMCSdCAw4pr7zQE=/1066x1066/smart/filters:no_upscale()/traditional-origami-boat-00-576cb3813df78cb62ca1cced.jpg";
+}  
+function eb()
+{
+    document.getElementById("w4").src="https://i.ytimg.com/vi/981t1yRjGFc/maxresdefault.jpg";
+}
+function f()
+{
+    document.getElementById("w5").src="https://i.ytimg.com/vi/hYWNDcERrdE/maxresdefault.jpg";
+}    
+function fb()
+{
+    document.getElementById("w5").src="https://i.ytimg.com/vi/nZGcl1VC4PU/hqdefault.jpg";
+}    
+function g()
+{
+    document.getElementById("w6").src="https://www.origamiway.com/images/planes/nakamura-lock/nakamura-lock.jpg";
+}
+function gb()
+{
+    document.getElementById("w6").src="https://i.ytimg.com/vi/zUM0_NqphW0/maxresdefault.jpg";
+}
